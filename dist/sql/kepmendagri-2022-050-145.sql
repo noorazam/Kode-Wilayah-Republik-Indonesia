ae01aed65f5e05d4e0995cdb79a593ea10758c5a
Moh Noor Al Azam <me@noorazam.my.id>
2023-07-17 11:33:09 +0700

    mengganti autoincrement ke angka 1

--- dist/sql/kepmendagri-2022-050-145.sql
+++ dist/sql/kepmendagri-2022-050-145.sql
@@ -28,7 +28,7 @@ CREATE TABLE `kodewil` (
   `nama_wil` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `kode_wil_UNIQUE` (`kode_wil`)
-) ENGINE=InnoDB AUTO_INCREMENT=90980 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
